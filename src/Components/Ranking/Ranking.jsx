b1d0983373e95c1dfa7698e2937f9aa9e9259208
Yure Araújo <yureesantos@gmail.com>
2019-04-28 04:39:21 -0300

    Correção design

--- src/Components/Ranking/Ranking.jsx
+++ src/Components/Ranking/Ranking.jsx
@@ -26,7 +26,6 @@ class Ranking extends Component {
   render() {
     let userRanked;
     let userPoints;
-    let userCravadas;
     if (this.state.users) {
       userRanked = this.state.users.map((user, index) => {
         return (
@@ -42,13 +41,6 @@ class Ranking extends Component {
           </div>
         );
       });
-      userCravadas = this.state.users.map((user, index) => {
-        return (
-          <div key={index} className={style.cravadas}>
-            {user.cravadas}
-          </div>
-        );
-      });
     }
 
     return (
@@ -59,10 +51,8 @@ class Ranking extends Component {
           <div className={style.rankingTable}>
             <span className={style.titles}>Jogador</span>
             <span className={style.titles}>Pontos</span>
-            <span className={style.titles}>Cravadas</span>
             <div>{userRanked}</div>
             <div>{userPoints}</div>
-            <div>{userCravadas}</div>
           </div>
         )}
       </div>
